faf6f4038a9ad22dc6f3bcfb9521a1ddd83620c3
zero-penguin <arato1060@outlook.jp>
2023-06-23 10:49:25 +0900

    データベースの作成

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,92 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__,
             static_folder="../frontend/dist/static",
             template_folder="../frontend/dist"
             )
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///admin.db'
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template("index.html")
+app.config['SECRET_KEY'] = 'secret_key'
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(15), nullable=False)
+    passward = db.Column(db.String(10), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    created_data = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class Users_names(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(15), nullable=False)
+    created_data = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class Users_password(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(15), nullable=False)
+    passward = db.Column(db.String(15), nullable=False)
+    created_data = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class Users_status(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    old = db.Column(db.Integer, nullable=False)
+    created_data= db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class Creates(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(10), nullable=False)
+    img = db.Column(db.String(10), nullable=False)
+    comment = db.Column(db.String(20), nullable=False)
+    goodcount = db.Column(db.String(50), nullable=False)
+    created_data= db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class Creates_title(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(10), nullable=False)
+    content = db.Column(db.String(300), nullable=False)
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class Creates_img(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    imgpath = db.VARCHAR(db.String(100))
+    created_data= db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class Creates_goodcount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    supporter_id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer, primary_key=True)
+    created_data= db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class Creates_comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    supporter_id = db.Column(db.Integer, primary_key=True)
+    supporter_name = db.Column(db.String(10), nullable=False)
+    supporter_content = db.Column(db.String(100), nullable=False)
+    created_data= db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+admin = Admin(app)
+admin.add_view(ModelView(Users,db.session))
+admin.add_view(ModelView(Users_names,db.session))
+admin.add_view(ModelView(Users_password,db.session))
+admin.add_view(ModelView(Users_status,db.session))
+admin.add_view(ModelView(Creates,db.session))
+admin.add_view(ModelView(Creates_title,db.session))
+admin.add_view(ModelView(Creates_img,db.session))
+admin.add_view(ModelView(Creates_goodcount,db.session))
+admin.add_view(ModelView(Creates_comment,db.session))
+
+app.route('/', defaults={'path': ''})
+def index(path):
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
